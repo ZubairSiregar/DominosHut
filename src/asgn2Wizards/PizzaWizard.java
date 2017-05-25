c6679483f8defcfc1b0b1e0bbfce4227313561e8
leiwang568 <leiwang.au@gmail.com>
2017-05-26 00:47:47 +1000

    update by Lei

--- src/asgn2Wizards/PizzaWizard.java
+++ src/asgn2Wizards/PizzaWizard.java
@@ -6,7 +6,7 @@ import javax.swing.SwingUtilities;
 import asgn2GUIs.PizzaGUI;
 
 /**
- * This class is the ‘entry point’ to the rest of the system and provides a public static void main method. 
+ * This class is the ï¿½entry pointï¿½ to the rest of the system and provides a public static void main method. 
  * At the moment, this just calls the asgn2GUIs.PizzaGUI class. You can probably leave the class as it is,
  *  however, you must make sure that it is the one and only entry point to the rest of the system. 
  *  
@@ -19,6 +19,7 @@ public class PizzaWizard {
      * Creates an instance of PizzaWizard
      */
 	public PizzaWizard() {
+		PizzaWizard wizard = new PizzaWizard();
 	}
 
 	/**
